0041474a932c7e1712627e8e6e9cd484fd694453
fennyflop <fennyflop@gmail.com>
2022-07-16 22:35:09 +0300

    added express js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 import express from 'express';
 
 const app = express()
-const port = 3000
+
+const port = 3000;
+
+const DIRNAME = process.cwd();  
+
+app.use("/", express.static(DIRNAME + "/dist"));
+
+app.get('/*', (req, res, next) => {
+  res.sendFile(DIRNAME +"/static/index.html");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
